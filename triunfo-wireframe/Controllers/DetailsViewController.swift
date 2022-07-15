d32b59562e6584e6262a0195a8e7f540ec7a2ad5
apfdamascena <apfd@cin.ufpe.br>
2022-07-15 15:13:14 -0300

    feature(gitignore): add TMDB.info

--- triunfo-wireframe/Controllers/DetailsViewController.swift
+++ triunfo-wireframe/Controllers/DetailsViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 class DetailsViewController: UIViewController {
     
     var movie: Movie?
-
+    @IBOutlet weak var backdropImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = movie?.title
+        guard let movie = movie else { return }
+        self.title = movie.title
+        backdropImage.image = UIImage(named: movie.backdrop)
+        titleLabel.text = movie.title
+        ratingLabel.text = "Rating: \(movie.voteAverage)/10"
+        posterImage.image = UIImage(named: movie.poster)
+        overviewLabel.text = movie.overview
     }
 }
